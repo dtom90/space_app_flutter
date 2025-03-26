dcc02f67ac7acdd2a924ca56b60d3eb7b156a7a6
David Thomason <dtom90@users.noreply.github.com>
2025-03-25 22:17:03 -0500

    adds flutter_earth_globe example

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-import './MyApp.dart';
+// import './MyApp.dart';
+import './GlobeApp.dart';
 
 void main() {
-  runApp(MyApp());
+  // runApp(MyApp());
+  runApp(MaterialApp(
+    title: 'Flutter Earth Globe',
+    theme: ThemeData(primarySwatch: Colors.blue),
+    debugShowCheckedModeBanner: false,
+    home: const GlobeApp(),
+  ));
 }
